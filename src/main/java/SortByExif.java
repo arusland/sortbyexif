f9b28ca8b90439a122117ec81455d303dce5a281
Ruslan A <ruslan.absalyamov@gmail.com>
2024-05-19 12:39:04 +0200

    Sort media-files by the year

---
+++ src/main/java/SortByExif.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Sort media files in a folder by the year taken from the EXIF data.
+ */
+public class SortByExif {
+    private final Path targetFolder;
+    // Parse string like "Date/Time Original              : 2022:02:12 11:25:42"
+    private static final Pattern PAT_DATE_TIME_ORIG = Pattern.compile("Date/Time Original\\s*:\\s*(\\d{4}):(\\d{2}):(\\d{2})");
+
+    public SortByExif(Path targetFolder) {
+        this.targetFolder = targetFolder;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("sortbyexif: move all media files in the target folder to subfolders by year taken");
+            System.out.println("Usage: sortbyexif <target folder>");
+            System.exit(1);
+        }
+        final Path targetFolder = Paths.get(args[0]).normalize().toAbsolutePath();
+        System.out.println("Starting to sort files in folder: " + targetFolder + "...");
+        try {
+            final int exitCode = new SortByExif(targetFolder).sort();
+            System.exit(exitCode);
+        } catch (final IOException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(10);
+        }
+    }
+
+    private int sort() throws IOException, InterruptedException {
+        if (!Files.exists(targetFolder)) {
+            System.out.println("Target folder does not exist: " + targetFolder);
+            return 2;
+        }
+        if (!Files.isDirectory(targetFolder)) {
+            System.out.println("Target folder is not a directory: " + targetFolder);
+            return 3;
+        }
+        final List<Path> allFiles = Files.list(targetFolder).filter(Files::isRegularFile).toList();
+        for (int i = 0; i < allFiles.size(); i++) {
+            final Path file = allFiles.get(i);
+            final int exitCode = sortFile(file);
+            if (exitCode != 0) {
+                System.out.println("Failed to sort file: " + file);
+                return exitCode;
+            }
+            final int percentage = (i + 1) * 100 / allFiles.size();
+            System.out.println(String.format("Processed %d of %d files: %s %%", i + 1, allFiles.size(), percentage));
+        }
+
+        return 0;
+    }
+
+    private int sortFile(final Path filePath) throws IOException, InterruptedException {
+        final StringWriter output = new StringWriter();
+        final int exitCode = runProgram(Arrays.asList("exiftool", filePath.toString()), output);
+        if (exitCode != 0) {
+            System.err.println("Failed to read EXIF data for file: " + filePath);
+            System.err.println(output);
+            return exitCode;
+        }
+
+        final Matcher matcher = PAT_DATE_TIME_ORIG.matcher(output.toString());
+        if (matcher.find()) {
+            final String year = matcher.group(1);
+            final Path targetFolder = this.targetFolder.resolve(year);
+            Files.createDirectories(targetFolder);
+            final Path targetPath = targetFolder.resolve(filePath.getFileName());
+            Files.move(filePath, targetPath);
+            System.out.println("Moved file to: " + targetPath);
+        } else {
+            System.out.println("Skip file: " + filePath);
+        }
+        return 0;
+    }
+
+    private int runProgram(final List<String> command, final Writer output) throws IOException, InterruptedException {
+        final ProcessBuilder processBuilder = new ProcessBuilder(command);
+        final Process process = processBuilder.start();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.write(line);
+            output.write("\n");
+        }
+
+        return process.waitFor();
+    }
+}
